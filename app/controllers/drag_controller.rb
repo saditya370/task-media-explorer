10413ce7a151b7e4f7c534ee30aaae88194a7c0f
Aditya Singh <saditya370@gmail.com>
2022-10-04 15:11:08 +0530

    Added drag controller and added draggable partial

--- app/controllers/drag_controller.rb
+++ app/controllers/drag_controller.rb
@@ -1,2 +1,14 @@
 class DragController < ApplicationController
+    def post 
+
+
+
+    end
+    
+
+    private
+
+    def drag_project_params 
+        params.require(:resource).permit(:id,:position)
+    end
 end
