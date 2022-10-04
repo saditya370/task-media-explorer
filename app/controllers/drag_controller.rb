9e3efede3c7d198a308e1b3b98caaad3a065f2e4
Aditya Singh <saditya370@gmail.com>
2022-10-04 18:59:39 +0530

    bootstrap

--- app/controllers/drag_controller.rb
+++ app/controllers/drag_controller.rb
@@ -2,16 +2,7 @@ class DragController < ApplicationController
     def post
         @post = Post.find(drag_post_params[:id])
         @post.insert_at(drag_post_params[:position].to_i + 1 )
-    
-
-    
     end 
-
-
-
-    
-    
-
     private
 
     def drag_post_params 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
     acts_as_list
     has_one_attached :image
+    scope :recent, -> { order(created_at: :desc) } 
 end
