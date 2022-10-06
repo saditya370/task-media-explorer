e747334b47f8bbf5d49b17c6a6524f1c196383e2
Aditya Singh <saditya370@gmail.com>
2022-10-06 12:44:51 +0530

    Added sorting to the same index page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,24 @@ class PostsController < ApplicationController
 
   # GET /posts or /posts.json
   def index
-    @posts = Post.all.order(position: :asc)
+    # @posts = Post.all.order(position: :asc)
+     @posts = Post.all.order(position: :asc)
+     
+     
+     @post = params[:check]
+     if @post == "0" 
+      @p =   @posts.sort_by(&:created_at)
+      
+    elsif @post == "1"
+      @p =   @posts.sort_by(&:created_at).reverse
+      
+    else 
+      @p = Post.all.order(position: :asc)
+      
+  end
+
+    
+  
   end
 
   # GET /posts/1 or /posts/1.json
